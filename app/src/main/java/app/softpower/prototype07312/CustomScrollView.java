14feaaf44f73262b86ada57dcc03357ba3795b69
cis <xhois1@gmail.com>
2022-08-27 20:46:52 +0900

    Initial commit

--- app/src/main/java/app/softpower/prototype07312/CustomScrollView.java
+++ app/src/main/java/app/softpower/prototype07312/CustomScrollView.java
@@ -1,48 +1,40 @@
 package app.softpower.prototype07312;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ScrollView;
 
 public class CustomScrollView extends ScrollView {
-    boolean isScrollViewSettingTop = true;
-    int oldY = -1;
-
 
     public CustomScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
-            @Override
-            public void onScrollChanged() {
-//                Log.i("cis", "getScrollY() " + String.valueOf(getScrollY()));
-                if (getScrollY() >= 1979){
-                    isScrollViewSettingTop = true;
-                } else {
-                    isScrollViewSettingTop = false;
-                }
-                Log.i("cis", "oldY: " + oldY + ", newY: " + getScrollY());
-                Log.i("cis", String.valueOf(isScrollViewSettingTop));
-                oldY = getScrollY();
-            }
-        });
     }
-
-//    @Override
-//    public boolean dispatchTouchEvent(MotionEvent ev) {
-//        if (!isScrollViewSettingTop) return false;
-//        else return true;
-//
-//    }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        Log.i("cis", String.valueOf(ev));
-        if (isScrollViewSettingTop) return false;
-        return true;
+    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        if (canScrollVertically(1)) {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+        super.onNestedPreScroll(target, dx, dy, consumed);
     }
 
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        if (canScrollVertically(1)) {
+            fling((int) velocityY);
+            return true;
+        }
+        return super.onNestedPreFling(target, velocityX, velocityY);
+    }
 
+    @Override
+    public void fling(int velocityY) {
+        super.fling(velocityY);
+    }
 }
